0addb0bc61cc4833d883c59df3fda2c76ea7a900
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-07-17 12:56:57 +0300

    O(n) and O(logn)

--- Big-O/Main.py
+++ Big-O/Main.py
@@ -1,8 +1,15 @@
 import math
 
+#  O(n)
+def n(number):
+    for i in range(1,n):
+        print(i)
+
+
+
+#  O(logn)
 def logN(number):
     while number>1:
         number=math.floor(number/2)
         print(number)
-
 logN(64)        
