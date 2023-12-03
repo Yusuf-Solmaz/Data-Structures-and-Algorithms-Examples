1b94972537b622727c829cc961f49d16ef0d4597
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-03 16:15:13 +0300

    Solution

--- Linked List/RemoveNthNode.py
+++ Linked List/RemoveNthNode.py
@@ -13,3 +13,48 @@ class Node():
         self.value=value
         self.next=None
         self.prev=None
+        
+        
+        
+firstNode = Node(1)
+secondNode = Node(2)
+thirdNode = Node(3)
+fourthNode = Node(4)   
+
+firstNode.next = secondNode
+secondNode.next = thirdNode
+
+secondNode.prev = firstNode
+thirdNode.prev = secondNode
+
+thirdNode.next=fourthNode
+fourthNode.prev=thirdNode  
+
+nodeList= []
+
+def remove (n:int,head:Node):
+
+    while(True):
+        if(head.next!=None):
+            nodeList.append(head.value)
+            head=head.next
+        else:
+            break
+    count = 1
+    nodeList.append(head.value)  
+    nodeList.pop(-n)
+    print(nodeList)
+    
+    for temp in nodeList:
+        node = Node(temp)
+        if(nodeList[count] != None):
+            node.next = nodeList[count]
+            count += 1
+        count=1   
+        if(nodeList[count] != None):
+            node.prev = nodeList[count-1]
+            count += 1
+        print(node.value)    
+    
+      
+remove(1,firstNode)        
